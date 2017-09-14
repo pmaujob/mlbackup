efce76bd8328ca728d7c0a93276696f3b009c7cd
dariostacruz <dariostacruz@gmail.com>
2017-09-14 17:13:07 -0500

    Signed-off-by: dariostacruz <dariostacruz@gmail.com>

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 @session_start();
-<<<<<<< HEAD
 require_once $pRoot.'/Libraries/SessionVars.php';
 $_SESSION['pRoot'] = dirname(__FILE__);
 $pRoot = $_SESSION['pRoot'];
@@ -30,38 +29,10 @@ if ($sess->exist() && $sess->varExist('user')==false) {
                             </div><div class="circle-clipper right">
                                 <div class="circle"></div>
                             </div>
-=======
-
-$_SESSION['pRoot'] = dirname(__FILE__);
-$pRoot = $_SESSION['pRoot'];
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Backups</title>
-        <?php include_once $pRoot . '/Views/header.php'; ?>
-        <script type="text/javascript" src="Views/js/login.js"></script>
-        <link rel="stylesheet" type="text/css" href="Views/css/stylesLogin.css">
-    </head>
-    <body onkeyup = "if (event.keyCode == 13) login();">
-        <div id="loading" class="loading">
-            <div class="logo-load">
-                <div class="preloader-wrapper big active valign-wrapper">
-                    <div class="spinner-layer spinner-blue-only">
-                        <div class="circle-clipper left">
-                            <div class="circle"></div>
-                        </div><div class="gap-patch">
-                            <div class="circle"></div>
-                        </div><div class="circle-clipper right">
-                            <div class="circle"></div>
->>>>>>> origin/master
                         </div>
                     </div>
                 </div>
             </div>
-<<<<<<< HEAD
             <div class="row">
                 <div class="col m3 l4"></div>
                 <div class="frm-login col s12 m6 l4 center-align">
@@ -101,37 +72,3 @@ $pRoot = $_SESSION['pRoot'];
      header("location: $route");
 }
 ?>
-=======
-        </div>
-        <div class="row">
-            <div class="col m3 l4"></div>
-            <div class="frm-login col s12 m6 l4 center-align">
-                <div class="row">
-                    <form>
-                        <div class="row">
-                            <div class="col s12">
-                                <div class="img">
-                                    <img src="Views/images/logoUser.png">
-                                </div>
-                                <h3 class="name-aplication">MlBackup</h3>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="in_user" type="text" class="validate">
-                                <label for="in_user">Usuario</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="in_pass" type="password" class="validate">
-                                <label for="in_pass">Contraseña</label>
-                            </div>
-                            <div class="col s12">
-                                <a class="waves-effect waves-light btn light-blue blueb" onclick="login();">Iniciar Sesión</a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div class="col m3 l4"></div>
-        </div>
-    </body>
-</html>
->>>>>>> origin/master
