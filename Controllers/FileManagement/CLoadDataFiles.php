f051b71bdc5a7b3eb891a2da470eafcadacef4fb
Mauricio <pmaujob@gmail.com>
2017-09-14 10:09:12 -0500

    Creación del proyecto

---
+++ Controllers/FileManagement/CLoadDataFiles.php
@@ -0,0 +1,77 @@
+<?php
+
+@session_start();
+
+$pRoot = $_SESSION['pRoot'];
+
+require_once $pRoot . '/Class/FileManagement/DataFiles.php';
+require_once $pRoot. '/Models/FileManagement/MLoadDataFiles.php';
+
+class CLoadDataFiles {
+
+    private $users = array();
+
+    private function getUsers() {
+        return $this->users;
+    }
+
+    public function setDataFilePasswd() {
+
+        try {
+
+            $file = fopen("passwd", "r") or exit("Unable to open file!");
+
+            while (!feof($file)) {
+
+                $search = "bk";
+                $str = fgets($file);
+                $resRow = strpos($str, $search);
+
+                if ($resRow !== false) {
+                    if (stristr($str, ':', true) !== 'adminbk')
+                        $this->users[] = stristr($str, ':', true);
+                }
+            }
+
+            fclose($file);
+        } catch (Exception $e) {
+            echo "Error: " . $e;
+        }
+    }
+    
+    public function recordData($area, $responsable, $ip, $account, $dir, $lastRecord, $hash, $lastIp, $directories, $deviceDirectorie){
+        
+        echo var_dump($area)."_1<br>";
+        echo var_dump($responsable)."_2<br>";
+        echo var_dump($ip)."_3<br>";
+        echo var_dump($account)."_4<br>";
+        echo var_dump($dir)."_5<br>";
+        echo var_dump($lastRecord)."_6<br>";
+        echo var_dump($hash)."_7<br>";
+        echo var_dump($lastIp)."_8<br>";
+        echo var_dump($directories)."_9<br>";
+        echo var_dump($deviceDirectorie)."_10<br><br><hr><br>";
+        
+    }
+
+    public function nextData() {
+
+        foreach ($this->getUsers() as $row){
+            
+            $dataFiles = new DataFiles();
+            $dataFiles->setData($row);
+            echo $row;
+            echo "array: ".var_dump($dataFiles->getDirectories());
+            $this->recordData($dataFiles->getArea(), $dataFiles->getResponsable(), $dataFiles->getIpsync(), $dataFiles->getAccount(), $dataFiles->getDirectory(), $dataFiles->getLastRecord(), $dataFiles->getHash(), $dataFiles->getLastIp(), $dataFiles->getDirectoriesJson(), $dataFiles->getDirConf());
+            
+        }
+        
+    }
+
+}
+
+$loadDataFiles = new CLoadDataFiles();
+$loadDataFiles->setDataFilePasswd();
+$loadDataFiles->nextData();
+
+?>
---
+++ Views/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <?php include_once 'header.php'; ?>
+        <style type="text/css">
+            #imgindex{
+                height: 700px;
+                display: -webkit-flex;
+                display:         flex;
+                -webkit-align-items: center;
+                align-items: center;
+                -webkit-justify-content: center;
+                justify-content: center;
+                text-align: center;
+            }
+        </style>
+
+    </head>
+    <body>
+
+        <?php include '../Views/menu.php'; ?>
+        <div class="container-fluid">
+            <div class="col s12 m8 l10">
+
+                <div class="row">
+                    <div class="col s12 m8 l12 center-align bajarimagen">
+<!--                        <div><img src="images/logoUser.png" width="300"></div>-->
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+    </div>
+    <div class="col s12 m8 l9">
+
+    </div>
+</body>
+</html>
